0b057541acf18fcdaed276094f485c509f1545dd
tshinowpub <wayfarermusic0130@gmail.com>
2019-10-22 07:24:05 +0900

    Gormの規則に従っていないテーブル構造であったため、テーブル構造を変更した

--- migrations/echo/000001_init.up.sql
+++ migrations/echo/000001_init.up.sql
@@ -4,6 +4,9 @@ CREATE TABLE IF NOT EXISTS echo.users (
     user_account_id VARCHAR(100) NOT NULL COMMENT 'アカウント作成時にユーザーが決めるID',
     user_name VARCHAR(100) NOT NULL COMMENT '任意のユーザー名',
     email text NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME,
+    deleted_at DATETIME,
     PRIMARY KEY (user_id),
     UNIQUE users_idx1 (user_account_id)
 ) ENGINE=InnoDB;
